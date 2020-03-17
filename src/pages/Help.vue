5836fae2c4b42685ed52c5d1907aad977610a58a
Stan <stasss95@mail.ru>
2020-03-17 10:57:14 +0300

    Transfer repo

---
+++ src/pages/Help.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit omnis in ea quam. At illo tempore magnam fuga aut quaerat magni accusantium sit dolore quia voluptatem, labore itaque, obcaecati dolores!</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
